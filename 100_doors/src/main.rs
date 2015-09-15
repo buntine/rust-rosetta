943ef9875edc826a5ada101c6521acd6e734fced
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-09-15 23:15:04 +1000

    Working simple version

--- 100_doors/src/main.rs
+++ 100_doors/src/main.rs
@@ -1,13 +1,18 @@
 fn main() {
     let mut doors = [false; 100];
+    let mut d;
     
     for p in 0..100 {
-        doors[p] = !doors[p];
+        d = p;
+        while d < doors.len() {
+            doors[d] = !doors[d];
+            d += (p + 1);
+        }
     }
 
-    for d in 0..doors.len() {
-        if doors[d] {
-            println!("{}", d + 1);
+    for (i, &d) in doors.iter().enumerate() {
+        if d {
+            println!("{}", i + 1);
         }
     }
 }
