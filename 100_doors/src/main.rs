022dcaa7671e1d4370f678f9d60767822a9e14ad
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-09-15 22:50:01 +1000

    One pass of doors

--- 100_doors/src/main.rs
+++ 100_doors/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let mut doors = [false; 100];
+    
+    for p in 0..100 {
+        doors[p] = !doors[p];
+    }
+
+    for d in 0..doors.len() {
+        if doors[d] {
+            println!("{}", d + 1);
+        }
+    }
 }
