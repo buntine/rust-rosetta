bb49d08908e2918bc76ca84309a8eff6338e7d67
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-09-16 22:39:39 +1000

    Printing actions

--- 24_game/src/main.rs
+++ 24_game/src/main.rs
@@ -1,6 +1,14 @@
 fn evaluate(program: Vec<char>) {
+    let stack: Vec<i32> = vec![];
+
     for c in program {
-        println!("{}", c);
+        match c {
+            '*' => println!("Multiply last two on stack"),
+            '+' => println!("Sum last two on stack"),
+            '-' => println!("Subtract last two on stack"),
+            '/' => println!("Divide last two on stack"),
+            _   => println!("Find digit -> Add to stack. Else -> Die"),
+        }
     }
 }
 
