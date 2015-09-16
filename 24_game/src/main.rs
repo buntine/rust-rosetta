3570f115188784c8249ccedb6427c0e13755614b
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-09-16 22:59:01 +1000

    moved into struct

--- 24_game/src/main.rs
+++ 24_game/src/main.rs
@@ -1,14 +1,53 @@
+struct Stack {
+    content: Vec<i32>,
+}
+
+impl Stack {
+    fn push(&mut self, d: i32) {
+        self.content.push(d);
+    }
+
+    fn multiply(&mut self) {
+        println!("Multiply");
+    }
+
+    fn add(&mut self) {
+        println!("Add");
+    }
+
+    fn subtract(&mut self) {
+        println!("Subtract");
+    }
+
+    fn divide(&mut self) {
+        println!("Divide");
+    }
+}
+
 fn evaluate(program: Vec<char>) {
-    let stack: Vec<i32> = vec![];
+    let mut stack = Stack { content: vec![] };
 
     for c in program {
         match c {
-            '*' => println!("Multiply last two on stack"),
-            '+' => println!("Sum last two on stack"),
-            '-' => println!("Subtract last two on stack"),
-            '/' => println!("Divide last two on stack"),
-            _   => println!("Find digit -> Add to stack. Else -> Die"),
+            '*' => stack.multiply(),
+            '+' => stack.add(),
+            '-' => stack.subtract(),
+            '/' => stack.divide(),
+            '1' => stack.push(1),
+            '2' => stack.push(2),
+            '3' => stack.push(3),
+            '4' => stack.push(4),
+            '5' => stack.push(5),
+            '6' => stack.push(6),
+            '7' => stack.push(7),
+            '8' => stack.push(8),
+            '9' => stack.push(9),
+            _   => println!("DIE..."),
         }
+    }
+
+    for c in stack.content {
+        println!("{}", c);
     }
 }
 
