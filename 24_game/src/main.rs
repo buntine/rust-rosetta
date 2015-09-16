09243e2320c3242222f6c0ac8011dd6b9b60ec08
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-09-16 22:15:16 +1000

    translating program from string into Vec<char>. took me an hour hahah

--- 24_game/src/main.rs
+++ 24_game/src/main.rs
@@ -1,2 +1,11 @@
+fn evaluate(program: Vec<char>) {
+    for c in program {
+        println!("{}", c);
+    }
+}
+
 fn main() {
+    let program: Vec<char> = "3 4 * 2 * 1 /".chars().filter(|&c| c != ' ').collect();
+
+    evaluate(program);
 }
