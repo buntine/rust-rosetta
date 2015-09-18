bb77e495a7c6e811296acfa5a53d14e0b7e60e56
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-09-18 22:44:51 +1000

    Refactor

--- 24_game/src/main.rs
+++ 24_game/src/main.rs
@@ -1,77 +1,34 @@
 struct Stack {
-    content: Vec<u32>,
+    content: Vec<i32>,
 }
 
 impl Stack {
-    fn push(&mut self, d: u32) -> Result<u32, &str> {
+    fn push(&mut self, d: i32) -> Result<i32, &str> {
         self.content.push(d);
         return Ok(d);
     }
 
-    fn pop(&mut self) -> Option<u32> {
+    fn pop(&mut self) -> Option<i32> {
         return self.content.pop();
     }
 
-    fn add_digit(&mut self, c: char) -> Result<u32, &str> {
+    fn add_digit(&mut self, c: char) -> Result<i32, &str> {
         match c.to_digit(10) {
-            Some(n) => self.push(n),
+            Some(n) => self.push(n as i32),
             None    => Err("Invalid character"),
         }
     }
 
-    fn multiply(&mut self) -> Result<u32, &str> {
+    fn apply<F>(&mut self, f: F) -> Result<i32, &str>
+        where F : Fn(i32, i32) -> i32 {
+
         let r = self.pop();
         let l = self.pop();
 
         match l {
             Some(n) => {
                 match r {
-                    Some(i) => self.push(n * i),
-                    None => Err("Stack empty"),
-                }
-            },
-            None => Err("Stack empty"),
-        }
-    }
-
-    fn add(&mut self) -> Result<u32, &str> {
-        let r = self.pop();
-        let l = self.pop();
-
-        match l {
-            Some(n) => {
-                match r {
-                    Some(i) => self.push(n * i),
-                    None => Err("Stack empty"),
-                }
-            },
-            None => Err("Stack empty"),
-        }
-    }
-
-    fn subtract(&mut self) -> Result<u32, &str> {
-        let r = self.pop();
-        let l = self.pop();
-
-        match l {
-            Some(n) => {
-                match r {
-                    Some(i) => self.push(n * i),
-                    None => Err("Stack empty"),
-                }
-            },
-            None => Err("Stack empty"),
-        }
-    }
-
-    fn divide(&mut self) -> Result<u32, &str> {
-        let r = self.pop();
-        let l = self.pop();
-
-        match l {
-            Some(n) => {
-                match r {
-                    Some(i) => self.push(n * i),
+                    Some(i) => self.push(f(n, i)),
                     None => Err("Stack empty"),
                 }
             },
@@ -80,31 +37,43 @@ impl Stack {
     }
 }
 
-fn evaluate(program: Vec<char>) {
+fn evaluate<'a>(program: Vec<char>) -> Result<i32, &'a str> {
     let mut stack = Stack { content: vec![] };
 
     for c in program {
-        let answer: Result<u32, &str> = match c {
-            '*' => stack.multiply(),
-            '+' => stack.add(),
-            '-' => stack.subtract(),
-            '/' => stack.divide(),
+        let answer: Result<i32, &str> = match c {
+            '*' => stack.apply(|l, r| l * r),
+            '+' => stack.apply(|l, r| l + r),
+            '-' => stack.apply(|l, r| l - r),
+            '/' => stack.apply(|l, r| l / r),
             _   => stack.add_digit(c),
         };
 
         if answer.is_err() {
-            println!("{}", answer.unwrap_err());
-            break;
+            println!("Actual: {}", answer.unwrap_err());
+            return Err("Ass");
         }
     }
 
-    for c in stack.content {
-        println!("{}", c);
+    match stack.content.len() {
+        1 => Ok(stack.content[0]),
+        _ => Err("Invalid formula"),
     }
 }
 
 fn main() {
     let program: Vec<char> = "3 4 * 2 * 1 /".chars().filter(|&c| c != ' ').collect();
 
-    evaluate(program);
+    match evaluate(program) {
+        Ok(n) => {
+            if n == 24 {
+                println!("Well done!");
+            } else {
+                println!("Nice try, but {} is not 24!", n);
+            }
+        },
+        Err(s) => {
+            println!("Oops: {}", s);
+        },
+    }
 }
