6ae740e39a825041ab73795306e0783aca0c7c72
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-09-20 16:36:06 +1000

    Started shell for ABC problem

--- abc_problem/src/main.rs
+++ abc_problem/src/main.rs
@@ -1,3 +1,14 @@
+fn can_spell(w: &str, blocks: &[&str]) -> bool {
+    true
+}
+
 fn main() {
-    println!("Hello, world!");
+    let words = ["A", "BARK", "BOOK", "TREAT", "COMMON", "SQUAD", "CONFUSE"];
+    let blocks = ["BO", "XK", "DQ", "CP", "NA", "GT", "RE",
+                  "TG", "QD", "FS", "JW", "HU", "VI", "AN",
+                  "OB", "ER", "FS", "LY", "PC", "ZM"];
+
+    for w in words.iter() {
+        println!("Can I spell {}: {}", w, can_spell(w, &blocks));
+    }
 }
