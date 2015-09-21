217e7c0a24ecbfa66183fed2d976ffb4b5a98100
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-09-21 20:21:21 +1000

    Started shell for balanced brackets

--- balanced_brackets/src/main.rs
+++ balanced_brackets/src/main.rs
@@ -1,3 +1,16 @@
+fn generate_expression(size: &i32) -> Vec<char> {
+    vec!['[', '[', ']', ']']
+}
+
+fn well_formed(expr: &Vec<char>) -> bool {
+    true
+}
+
 fn main() {
-    println!("Hello, world!");
+    for n in (1..10) {
+        let expr = generate_expression(&n);
+        let wf = well_formed(&expr);
+
+        println!("{:?} is {}", expr, if wf {"well formed"} else {"NOT well formed"});
+    }
 }
