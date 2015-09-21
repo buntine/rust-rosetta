fa4b792e4352b43b7897e96934e9ca7352a8cd01
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-09-21 21:57:43 +1000

    Generating vecs of n [ and n ]

--- balanced_brackets/src/main.rs
+++ balanced_brackets/src/main.rs
@@ -3,9 +3,9 @@ use std::iter::repeat;
 fn generate_expression(size: &usize) -> Vec<char> {
     let opens = repeat('[').take(*size);
     let closes = repeat(']').take(*size);
-    let expr = opens.chain(&closes).collect(); 
+    let expr = opens.chain(closes);
 
-    expr
+    expr.collect()
 }
 
 fn well_formed(expr: &Vec<char>) -> bool {
@@ -29,10 +29,9 @@ fn main() {
     for n in (1..10) {
         let expr = generate_expression(&n);
         let wf = well_formed(&expr);
-        let wf_str: String = expr.into_iter().collect();
 
         println!("'{}' is {}",
-                 wf_str,
+                 expr.into_iter().collect::<String>(),
                  if wf {"well formed"} else {"NOT well formed"});
     }
 }
