1a974a3592c08f28dcb9f324723846a4f348a601
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-09-22 23:16:07 +1000

    Added step and report

--- toy_robot/src/main.rs
+++ toy_robot/src/main.rs
@@ -19,6 +19,20 @@ struct Robot {
     t: Table,
 }
 
+impl Direction {
+    fn string(&self) -> String {
+        let s = match *self { 
+            Direction::North => "North",
+            Direction::East => "East",
+            Direction::South => "South",
+            Direction::West => "West",
+            _ => "Empty",
+        };
+
+        s.to_string()
+    }
+}
+
 impl Default for Robot {
     fn default() -> Robot {
         let table = Table{w: 5, h: 5};
@@ -34,10 +48,6 @@ impl Table {
 }
 
 impl Robot {
-    fn is_placed(&self) -> bool {
-        self.d == Direction::Empty
-    }
-
     fn place(&mut self, x: i32, y: i32, d: Direction) {
         if self.t.valid_pos(x, y) {
             self.x = x;
@@ -71,9 +81,17 @@ impl Robot {
     }
 
     fn step(&mut self) {
+        match self.d {
+            Direction::North => if self.t.valid_pos(self.x, self.y - 1) {self.y -= 1;},
+            Direction::East => if self.t.valid_pos(self.x + 1, self.y) {self.x += 1;},
+            Direction::South => if self.t.valid_pos(self.x, self.y + 1) {self.y += 1;},
+            Direction::West => if self.t.valid_pos(self.x - 1, self.y) {self.y -= 1;},
+            _ => (),
+        }
     }
 
     fn report(&self) {
+        println!("{}, {}, {}", self.x, self.y, self.d.string());
     }
 }
 
