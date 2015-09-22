b872047e6c551614cdee32ee69e3eb2923b58735
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-09-22 22:44:09 +1000

    Implementing robot methods

--- toy_robot/src/main.rs
+++ toy_robot/src/main.rs
@@ -6,26 +6,53 @@ enum Direction {
     Empty,
 }
 
+struct Table {
+    w: i32,
+    h: i32,
+}
+
 struct Robot {
     x: i32,
     y: i32,
     d: Direction,
+    t: Table,
 }
 
 impl Default for Robot {
     fn default() -> Robot {
-        Robot{x: 0, y: 0, d: Direction::Empty}
+        let table = Table{w: 5, h: 5};
+
+        Robot{x: 0, y: 0, d: Direction::Empty, t: table}
+    }
+}
+
+impl Table {
+    fn valid_pos(&self, x: i32, y: i32) -> bool {
+        x >= 0 && x < self.w && y >= 0 && y < self.h
     }
 }
 
 impl Robot {
-    fn place(&self, x: i32, y: i32, d: Direction) {
-        println!("Woohoo");
+    fn place(&mut self, x: i32, y: i32, d: Direction) {
+        if self.t.valid_pos(x, y) {
+            self.x = x;
+            self.y = y;
+            self.d = d;
+        }
+    }
+
+    fn left(&mut self) {
+    }
+
+    fn right(&mut self) {
+    }
+
+    fn report(&self) {
     }
 }
 
 fn main() {
-    let robot: Robot = Default::default();
+    let mut robot: Robot = Default::default();
 
     robot.place(1, 2, Direction::North);
 }
