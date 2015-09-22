890786d4dfef9b2126e33d7d1ae6321c9e838e84
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-09-22 22:24:02 +1000

    Started structure for toy robot

--- toy_robot/src/main.rs
+++ toy_robot/src/main.rs
@@ -1,3 +1,31 @@
+enum Direction {
+    North,
+    East,
+    South,
+    West,
+    Empty,
+}
+
+struct Robot {
+    x: i32,
+    y: i32,
+    d: Direction,
+}
+
+impl Default for Robot {
+    fn default() -> Robot {
+        Robot{x: 0, y: 0, d: Direction::Empty}
+    }
+}
+
+impl Robot {
+    fn place(&self, x: i32, y: i32, d: Direction) {
+        println!("Woohoo");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let robot: Robot = Default::default();
+
+    robot.place(1, 2, Direction::North);
 }
