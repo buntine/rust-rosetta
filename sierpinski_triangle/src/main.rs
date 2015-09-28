e3bb9528339901ee8c751065098ee9335dbebf64
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-09-28 22:59:38 +1000

    Adding in piston pattern

--- sierpinski_triangle/src/main.rs
+++ sierpinski_triangle/src/main.rs
@@ -9,6 +9,31 @@ use piston::input::*;
 use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{ GlGraphics, OpenGL };
 
+struct App {
+    gl: GlGraphics,
+}
+
+impl App {
+    fn render(&mut self, args: &RenderArgs) {
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opengl = OpenGL::V3_2;
+
+    let window: Window = WindowSettings::new(
+        "Sierpinski-Triangle",
+        [800, 600])
+        .opengl(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    let mut app = App{gl: GlGraphics::new(opengl)};
+
+    for e in window.events() {
+        if let Some(r) = e.render_args() {
+            app.render(&r);
+        }
+    }
 }
