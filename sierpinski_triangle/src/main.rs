3aaea25e92793c692fd4862ddabea8aa8ce7cb04
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-09-30 21:47:24 +1000

    Generating vertices for sub-triangles

--- sierpinski_triangle/src/main.rs
+++ sierpinski_triangle/src/main.rs
@@ -35,24 +35,33 @@ impl App {
     }
 }
 
+fn sub_triangles(v: [[f64; 2]; 4]) -> Vec<[[f64; 2]; 4]> {
+    let (a, b, c, d) = (v[0], v[1], v[2], v[3]); // Vector destructuring is not yet stable.
+
+    vec![
+        [[a[0] + ((b[0] - a[0]) / 2.0), a[1]],
+         [b[0] - ((b[0] - c[0]) / 2.0), b[1] - ((b[1] - c[1]) / 2.0)],
+         [a[0] + ((c[0] - a[0]) / 2.0), a[1] - ((a[1] - c[1]) / 2.0)],
+         [a[0] + ((b[0] - a[0]) / 2.0), a[1]]],
+        [[100.0, 200.0], [200.0, 200.0], [150.0, 100.0], [100.0, 200.0]],
+        [[100.0, 200.0], [200.0, 200.0], [150.0, 100.0], [100.0, 200.0]],
+        [[100.0, 200.0], [200.0, 200.0], [150.0, 100.0], [100.0, 200.0]]]
+}
+
 fn sierpinski(depth: i32, vertices: [[f64; 2]; 4]) -> Vec<[[f64; 2]; 4]> {
     let mut triangles = vec![];
 
     match depth {
         0 => triangles,
         d @ _ => {
-            // TODO: Break into four triangles. Push first. And then recurse on the rest.
-            triangles.push([[100.0, 200.0], [200.0, 200.0],
-                            [150.0, 100.0], [100.0, 200.0]]);
+            let sub_tris = sub_triangles(vertices);
 
-            for t in sierpinski(d - 1, [[100.0, 200.0], [200.0, 200.0], [150.0, 100.0], [100.0, 200.0]]) {
-                triangles.push(t);
-            }
-            for t in sierpinski(d - 1, [[100.0, 200.0], [200.0, 200.0], [150.0, 100.0], [100.0, 200.0]]) {
-                triangles.push(t);
-            }
-            for t in sierpinski(d - 1, [[100.0, 200.0], [200.0, 200.0], [150.0, 100.0], [100.0, 200.0]]) {
-                triangles.push(t);
+            triangles.push(sub_tris[0]);
+
+            for sub_tri in &sub_tris[1..] {
+                for t in sierpinski(d - 1, *sub_tri) {
+                    triangles.push(t);
+                }
             }
 
             triangles
@@ -71,8 +80,8 @@ fn main() {
         .build()
         .unwrap();
 
-    let triangles = sierpinski(4, [[100.0, 200.0], [200.0, 200.0],
-                                   [150.0, 100.0], [100.0, 200.0]]);
+    let triangles = sierpinski(4, [[50.0, 750.0], [950.0, 750.0],
+                                   [500.0, 50.0], [50.0, 750.0]]);
 
     let mut app = App{gl: GlGraphics::new(opengl), triangles: triangles};
 
