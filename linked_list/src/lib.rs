f3b915c19b825227a89af4831861a6bab3bfae80
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-12-12 21:07:11 +1100

    LinkedList with insert working

--- linked_list/src/lib.rs
+++ linked_list/src/lib.rs
@@ -14,19 +14,31 @@ impl<T> LinkedList<T> {
         LinkedList{head: None}
     }
 
-    pub fn insert(&mut self, value: T) -> LinkedList<T> {
+    pub fn insert(&mut self, value: T) {
         let h = match self.head.take() {
-            Some(n) => Some(Link{value: value, next: Some(Box::new(n))}),
+            Some(n) => Some(Box::new(n)),
             None => None,
         };
 
-        LinkedList{head: h}
+        self.head = Some(Link{value: value, next: h})
+    }
+
+    pub fn first(&self) -> Option<&T> {
+        match self.head {
+            Some(ref h) => Some(&h.value),
+            None => None,
+        }
     }
 }
 
 #[test]
 fn it_works() {
     let mut ll: LinkedList<i32> = LinkedList::new();
+    assert_eq!(ll.first(), None);
+
     ll.insert(1);
+    assert_eq!(ll.first(), Some(&1));
+
     ll.insert(2);
+    assert_eq!(ll.first(), Some(&2));
 }
