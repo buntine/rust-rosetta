947f73adceefd3918341a0fe7eeae89970d4cc52
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-12-13 22:41:55 +1100

    Implemented removal for linkedlist

--- linked_list/src/lib.rs
+++ linked_list/src/lib.rs
@@ -10,6 +10,14 @@ struct LinkedList<T> {
 }
 
 impl<T> LinkedList<T> {
+    fn lookup<'a>(&'a self, base: &'a Node<T>, offset: usize) -> Option<&Box<Link<T>>> {
+        match *base {
+            Some(ref n) if offset == 0 => Some(n),
+            Some(ref n) => self.lookup(&n.next, offset - 1),
+            _ => None,
+        }
+    }
+
     pub fn new() -> LinkedList<T> {
         LinkedList{head: None}
     }
@@ -19,16 +27,18 @@ impl<T> LinkedList<T> {
         self.head = Some(Box::new(Link{value: value, next: h}))
     }
 
-    fn lookup<'a>(&'a self, base: &'a Node<T>, offset: usize) -> Option<&T> {
-        match *base {
-            Some(ref h) if offset == 0 => Some(&h.value),
-            Some(ref h) => self.lookup(&h.next, offset - 1),
-            _ => None,
+    pub fn remove(&mut self) {
+        match self.head.take() {
+            Some(h) => { self.head = h.next },
+            None => { }
         }
     }
 
     pub fn get(&self, index: usize) -> Option<&T> {
-        self.lookup(&self.head, index)
+        match self.lookup(&self.head, index) {
+            Some(ref n) => Some(&n.value),
+            None => None,
+        }
     }
 }
 
@@ -48,6 +58,21 @@ fn insertion() {
 
     ll.insert(2);
     assert_eq!(ll.get(0), Some(&2));
+}
+
+#[test]
+fn removal() {
+    let mut ll: LinkedList<i32> = LinkedList::new();
+    ll.insert(1);
+    ll.insert(2);
+
+    assert_eq!(ll.get(0), Some(&2));
+
+    ll.remove();
+    assert_eq!(ll.get(0), Some(&1));
+
+    ll.remove();
+    assert_eq!(ll.get(0), None);
 }
 
 #[test]
