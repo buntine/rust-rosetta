0fac000f3713248a5880490723a6e2e30afd328f
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-12-13 22:13:43 +1100

    Replace first with lookup in linkedlist

--- linked_list/src/lib.rs
+++ linked_list/src/lib.rs
@@ -6,7 +6,7 @@ struct Link<T> {
 }
 
 struct LinkedList<T> {
-    head: Option<Link<T>>,
+    head: Node<T>,
 }
 
 impl<T> LinkedList<T> {
@@ -15,30 +15,50 @@ impl<T> LinkedList<T> {
     }
 
     pub fn insert(&mut self, value: T) {
-        let h = match self.head.take() {
-            Some(n) => Some(Box::new(n)),
-            None => None,
-        };
-
-        self.head = Some(Link{value: value, next: h})
+        let h = self.head.take();
+        self.head = Some(Box::new(Link{value: value, next: h}))
     }
 
-    pub fn first(&self) -> Option<&T> {
-        match self.head {
-            Some(ref h) => Some(&h.value),
-            None => None,
+    fn lookup<'a>(&'a self, base: &'a Node<T>, offset: usize) -> Option<&T> {
+        match *base {
+            Some(ref h) if offset == 0 => Some(&h.value),
+            Some(ref h) => self.lookup(&h.next, offset - 1),
+            _ => None,
         }
+    }
+
+    pub fn get(&self, index: usize) -> Option<&T> {
+        self.lookup(&self.head, index)
     }
 }
 
 #[test]
 fn it_works() {
     let mut ll: LinkedList<i32> = LinkedList::new();
-    assert_eq!(ll.first(), None);
+    assert_eq!(ll.get(0), None);
+}
+
+#[test]
+fn insertion() {
+    let mut ll: LinkedList<i32> = LinkedList::new();
+    assert_eq!(ll.get(0), None);
 
     ll.insert(1);
-    assert_eq!(ll.first(), Some(&1));
+    assert_eq!(ll.get(0), Some(&1));
 
     ll.insert(2);
-    assert_eq!(ll.first(), Some(&2));
+    assert_eq!(ll.get(0), Some(&2));
+}
+
+#[test]
+fn lookup() {
+    let mut ll: LinkedList<i32> = LinkedList::new();
+    ll.insert(3);
+    ll.insert(2);
+    ll.insert(1);
+
+    assert_eq!(ll.get(0), Some(&1));
+    assert_eq!(ll.get(1), Some(&2));
+    assert_eq!(ll.get(2), Some(&3));
+    assert_eq!(ll.get(3), None);
 }
