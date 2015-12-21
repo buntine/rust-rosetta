ed497e1bec25a25c7a97ad24d33400fb4b5f1b8b
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-12-21 23:49:16 +1100

    Added some notes for blog post

--- lifetimes/src/lib.rs
+++ lifetimes/src/lib.rs
@@ -27,3 +27,14 @@ fn it_works() {
     assert_eq!(tom.parents_name(), Some("Jane"));
     assert_eq!(jane.parents_name(), None);
 }
+
+// - We are making a promise to the compiler that all of these things live for atleast the same scope.
+// - We are not asking the compiler to "create a lifetime". We are just telling it to complain if we
+//   don't do what we say we are doing.
+// - Why? Because this allows Rust to ensure we cannot cause common memory allocation bugs. It does
+//   this at compile time. It's a zero-cost abstraction because there is no runtime penalty.
+// - It makes code look "uglier". But this is the wrong mindset for writing systems-level programs.
+//   The emphasis here is on safety, not expressiveness.
+// - Question: Why can't Rust ensure/infer/elide this scenario? Surely any reference to a Parent
+//   must *always* live for the same or a bigger scope???
+// - 
