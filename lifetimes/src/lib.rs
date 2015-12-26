9c1766ad3b4a397cc5c00b45c34820332acae910
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-12-26 23:18:04 +1100

    An exmaple of the static lifetime

--- lifetimes/src/lib.rs
+++ lifetimes/src/lib.rs
@@ -29,6 +29,15 @@ fn double_first_five<'a>(a: &'a String, b: &'a String) -> (&'a str, &'a str) {
     (&a[..5], &b[..5])
 }
 
+// Output borrows generally must have an input value from which it can borrow. The exception is
+// the static lifetime.
+fn output_only<'a>() -> (&'static u8, &'static u8) {
+    static X: u8 = 8;
+    static Y: u8 = 9;
+
+    (&X, &Y)
+}
+
 // Here we are promising Rust that all of the borrowed u8's
 // live for the same scope.
 // Q: Why do I need a lifetime here?
@@ -51,6 +60,8 @@ fn it_works() {
     let arr_ages = vec![[Age(90), Age(80)],
                         [Age(2), Age(45)]];
 
+    assert_eq!(output_only(), (&8, &9));
+
     assert_eq!(first_five(&name), "Andre");
     assert_eq!(double_first_five(&name, &another), ("Andre", "Timot"));
 
@@ -71,7 +82,8 @@ fn it_works() {
 //   this at compile time. It's a zero-cost abstraction because there is no runtime penalty.
 // - It makes code look "uglier". But this is the wrong mindset for writing systems-level programs.
 //   The emphasis here is on safety, not expressiveness.
-// - Question: Why can't Rust ensure/infer/elide this scenario? Surely any reference to a Parent
+// - When we say "'a", we are talking about a scope in the calling environment.
+// - Question: Why can't Rust ensure/infer/elide the struct scenario? Surely any reference to a Parent
 //   must *always* live for the same or a bigger scope???
 //   - There is an open RFC for this
 //   - It may be complex to implement and/or slow down compilation
