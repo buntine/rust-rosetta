0b29fbeccc7023696c2001d48700154532dee114
Andrew Buntine Of Doom <bunts@hhd.com.au>
2016-01-08 00:09:54 +1100

    Fleshing out general logic for iterator

--- rgb/src/main.rs
+++ rgb/src/main.rs
@@ -1,6 +1,5 @@
 struct Gradient {
     start: [i8; 3],
-    stop: [i8; 3],
     steps: [i8; 3],
     n: i32,
     total: i32,
@@ -10,7 +9,6 @@ impl Gradient {
     fn new(start: String, stop: String, steps: i32) -> Gradient {
         Gradient{
             start: [0,0,0],
-            stop: [255,255,255],
             steps: [17,17,17],
             n: 0,
             total: 16
@@ -25,7 +23,13 @@ impl Iterator for Gradient {
         match self.n {
             n if n == self.total => None,
             n => {
-                Some(format!("{:x}{:02x}{:x}", 255, 0, 255))
+                let r = self.start[0] as i32 + (self.steps[0] as i32 * n);
+                let g = self.start[1] as i32 + (self.steps[1] as i32 * n);
+                let b = self.start[2] as i32 + (self.steps[2] as i32 * n);
+
+                self.n += 1;
+
+                Some(format!("{:02x}{:02x}{:02x}", r, g, b))
             }
         }
     }
@@ -35,9 +39,13 @@ impl Iterator for Gradient {
 fn it_works() {
   let g = Gradient::new("000000".to_string(), "ffffff".to_string(), 16);
 
-  assert_eq!(g.take(2).collect::<Vec<String>>(), vec!["ff00ff".to_string(), "ff00ff".to_string()]);
+  assert_eq!(g.take(2).collect::<Vec<String>>(), vec!["000000".to_string(), "111111".to_string()]);
 }
 
 fn main() {
-    println!("Hello, world!");
+  let g = Gradient::new("000000".to_string(), "ffffff".to_string(), 16);
+
+  for c in g {
+      println!("{}", c);
+  }
 }
