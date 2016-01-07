9f062973f477e1229f95eeafcc6c096891eaed61
Andrew Buntine Of Doom <bunts@hhd.com.au>
2016-01-08 00:01:42 +1100

    Fleshing out next() method

--- rgb/src/main.rs
+++ rgb/src/main.rs
@@ -9,9 +9,9 @@ struct Gradient {
 impl Gradient {
     fn new(start: String, stop: String, steps: i32) -> Gradient {
         Gradient{
-            start: [1,2,3],
-            stop: [1,2,3],
-            steps: [1,2,3],
+            start: [0,0,0],
+            stop: [255,255,255],
+            steps: [17,17,17],
             n: 0,
             total: 16
         }
@@ -22,16 +22,20 @@ impl Iterator for Gradient {
     type Item = String;
 
     fn next(&mut self) -> Option<String> {
-        self.n += 1;
-        Some("FF00FF".to_string())
+        match self.n {
+            n if n == self.total => None,
+            n => {
+                Some(format!("{:x}{:02x}{:x}", 255, 0, 255))
+            }
+        }
     }
 }
 
 #[test]
 fn it_works() {
-  let g = Gradient::new("000000".to_string(), "FFFFFF".to_string(), 16);
+  let g = Gradient::new("000000".to_string(), "ffffff".to_string(), 16);
 
-  assert_eq!(g.take(2).collect::<Vec<String>>(), vec!["FF00FF".to_string(), "FF00FF".to_string()]);
+  assert_eq!(g.take(2).collect::<Vec<String>>(), vec!["ff00ff".to_string(), "ff00ff".to_string()]);
 }
 
 fn main() {
