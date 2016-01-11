024a5dcf99be0e1b2a4947ba9ae683b8e4532284
Andrew Buntine Of Doom <bunts@hhd.com.au>
2016-01-12 01:01:45 +1100

    got it working. Probably should refactor this

--- rgb/src/main.rs
+++ rgb/src/main.rs
@@ -1,31 +1,28 @@
-extern crate regex;
-
-use regex::Regex;
-
 struct Gradient {
-    start: Vec<u32>,
-    steps: Vec<u32>,
-    n: u32,
-    total: u32,
+    start: Vec<i32>,
+    steps: Vec<f32>,
+    n: i32,
+    total: i32,
 }
 
 impl Gradient {
-    fn to_parts(hex: &str) -> Vec<u32> {
-        let re = Regex::new(r"[0-9a-fA-F]{2}").unwrap();
-
-        re.split(&hex)
-          .map(|c| u32::from_str_radix(c, 16).unwrap_or(0))
-          .collect()
+    fn to_parts(hex: &str) -> Vec<i32> {
+        [0, 2, 4].iter()
+                 .map(|&n| i32::from_str_radix(&hex[n..(n + 2)], 16).unwrap())
+                 .collect()
     }
 
-    fn to_steps(start: &[u32], stop: &[u32]) -> Vec<u32> {
-        vec![17, 17, 17]
+    fn to_steps(start: &[i32], stop: &[i32], n: i32) -> Vec<f32> {
+        start.iter()
+             .zip(stop.iter())
+             .map(|(&a, &b)| (a as f32 - b as f32) / (n as f32 - 1.0))
+             .collect()
     }
 
-    pub fn new(start: String, stop: String, n: u32) -> Gradient {
-        let from = Gradient::to_parts(&start[..]);
-        let to = Gradient::to_parts(&stop[..]);
-        let steps = Gradient::to_steps(&from[..], &to[..]);
+    pub fn new(start: &str, stop: &str, n: i32) -> Gradient {
+        let from = Gradient::to_parts(&start);
+        let to = Gradient::to_parts(&stop);
+        let steps = Gradient::to_steps(&from[..], &to[..], n);
 
         Gradient{
             start: from,
@@ -46,7 +43,7 @@ impl Iterator for Gradient {
                 let rgb: String = self.start
                     .iter()
                     .zip(self.steps.iter())
-                    .map(|(&a, &b)| a + (b * n))
+                    .map(|(&a, &b)| a - (n as f32 * b) as i32)
                     .map(|i| format!("{:02x}", i))
                     .collect();
 
@@ -59,18 +56,29 @@ impl Iterator for Gradient {
 }
 
 fn main() {
-    let g = Gradient::new("000000".to_string(), "ffffff".to_string(), 10000);
+    let g = Gradient::new("000000", "ffffff", 10000);
 
     g.collect::<Vec<String>>();
 }
 
 #[test]
 fn it_works() {
-    let g = Gradient::new("000000".to_string(), "ffffff".to_string(), 16);
-    let result = vec!["000000", "111111", "222222", "333333", "444444",
-                      "555555", "666666", "777777", "888888", "999999",
-                      "aaaaaa", "bbbbbb", "cccccc", "dddddd", "eeeeee",
-                      "ffffff"];
+    let ga = Gradient::new("000000", "ffffff", 16);
+    let gb = Gradient::new("ff334c", "d8ff1a", 10);
+    let result_a = vec!["000000", "111111", "222222", "333333", "444444",
+                        "555555", "666666", "777777", "888888", "999999",
+                        "aaaaaa", "bbbbbb", "cccccc", "dddddd", "eeeeee",
+                        "ffffff"];
+    let result_b = vec!["ff334c", "fb4947", "f76041", "f2773c", "ee8d36",
+                        "eaa431", "e5bb2b", "e1d126", "dde820", "d8ff1a"];
 
-    assert_eq!(g.collect::<Vec<String>>(), result.iter().map(|r| r.to_string()).collect::<Vec<String>>());
-}
+    assert_eq!(ga.collect::<Vec<String>>(), result_a.iter().map(|r| r.to_string()).collect::<Vec<String>>());
+    assert_eq!(gb.collect::<Vec<String>>(), result_b.iter().map(|r| r.to_string()).collect::<Vec<String>>());
+
+    let a = Gradient::to_parts("000000");
+    let b = Gradient::to_parts("ffffff");
+    assert_eq!(Gradient::to_steps(&a[..], &b[..], 16), vec![-17.0, -17.0, -17.0]);
+
+    assert_eq!(Gradient::to_parts("000000"), vec![0, 0, 0]);
+    assert_eq!(Gradient::to_parts("ffffff"), vec![255, 255, 255]);
+} 
