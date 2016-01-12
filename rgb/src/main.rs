ae1fce95f4d6c99f2bc68df99cd31e4b71ff6b22
Andrew Buntine Of Doom <bunts@hhd.com.au>
2016-01-12 23:02:41 +1100

    ADded unitlike struct that generates GradientIters

--- rgb/src/main.rs
+++ rgb/src/main.rs
@@ -1,4 +1,5 @@
-struct Gradient {
+struct Gradient;
+struct GradientIter {
     start: Vec<i32>,
     steps: Vec<f32>,
     n: i32,
@@ -6,25 +7,25 @@ struct Gradient {
 }
 
 impl Gradient {
-    fn to_parts(hex: &str) -> Vec<i32> {
+    fn to_parts(&self, hex: &str) -> Vec<i32> {
         [0, 2, 4].iter()
                  .map(|&n| i32::from_str_radix(&hex[n..(n + 2)], 16).unwrap())
                  .collect()
     }
 
-    fn to_steps(start: &[i32], stop: &[i32], n: i32) -> Vec<f32> {
+    fn to_steps(&self, start: &[i32], stop: &[i32], n: i32) -> Vec<f32> {
         start.iter()
              .zip(stop.iter())
              .map(|(&a, &b)| (a - b) as f32 / (n as f32 - 1.0))
              .collect()
     }
 
-    pub fn new(start: &str, stop: &str, n: i32) -> Gradient {
-        let from = Gradient::to_parts(&start);
-        let to = Gradient::to_parts(&stop);
-        let steps = Gradient::to_steps(&from[..], &to[..], n);
+    fn gradient_iter(&self, start: &str, stop: &str, n: i32) -> GradientIter {
+        let from = self.to_parts(&start);
+        let to = self.to_parts(&stop);
+        let steps = self.to_steps(&from[..], &to[..], n);
 
-        Gradient{
+        GradientIter{
             start: from,
             steps: steps,
             n: 0,
@@ -33,7 +34,7 @@ impl Gradient {
     }
 }
 
-impl Iterator for Gradient {
+impl Iterator for GradientIter {
     type Item = String;
 
     fn next(&mut self) -> Option<String> {
@@ -57,14 +58,15 @@ impl Iterator for Gradient {
 
 fn main() {
     // For benchmarking...
-    let g = Gradient::new("000000", "ffffff", 10000);
-    g.collect::<Vec<String>>();
+    let ga = Gradient.gradient_iter("000000", "ffffff", 10000);
+    ga.collect::<Vec<String>>();
 }
 
 #[test]
 fn it_works() {
-    let ga = Gradient::new("000000", "ffffff", 16);
-    let gb = Gradient::new("ff334c", "d8ff1a", 10);
+    let g = Gradient;
+    let ga = g.gradient_iter("000000", "ffffff", 16);
+    let gb = g.gradient_iter("ff334c", "d8ff1a", 10);
     let result_a = vec!["000000", "111111", "222222", "333333", "444444",
                         "555555", "666666", "777777", "888888", "999999",
                         "aaaaaa", "bbbbbb", "cccccc", "dddddd", "eeeeee",
